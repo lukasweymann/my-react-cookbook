b313f325351a0c52eebc731a590df59e05df5a3d
Lukas <lukas_weymann@hotmail.com>
2020-06-20 11:51:20 +0200

    Added rough sketch of the cookbook landing page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./HomePage.css";
+
+function HomePage() {
+  return (
+    <div className="main-container_homepage">
+      <nav>
+        <a>HOME</a>
+        <a>ABOUT</a>
+        <a>SURPRISE ME!</a>
+        <a>WHAT TO COOK?</a>
+      </nav>
+      <div className="wrapper-homepage">
+        <div className="headers-homepage">
+          <h1>WE LOVE CAFFEINE</h1>
+          <h2>WE CERTAINLY NEED IT TO COME TO A HUMAN-LIKE STATE</h2>
+        </div>
+        <div className="image-container_homepage">
+          <img src="#" alt="coffe mug"></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default HomePage;
