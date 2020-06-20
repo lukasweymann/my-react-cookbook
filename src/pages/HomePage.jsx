6117a1be1506d3a01a43a15731234877aaf2a069
Hanna Christ <christh87@googlemail.com>
2020-06-20 13:15:28 +0200

    Add image, update styles and nav

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,16 +8,17 @@ function HomePage() {
         <a>HOME</a>
         <a>ABOUT</a>
         <a>SURPRISE ME!</a>
-        <a>WHAT TO COOK?</a>
+        <a>PLACES</a>
+        <a>COFFEE AND SLEEP</a>
+        <a>ESSENTIALS</a>
+        <a>HOW TO BREW</a>
       </nav>
       <div className="wrapper-homepage">
         <div className="headers-homepage">
           <h1>WE LOVE CAFFEINE</h1>
           <h2>WE CERTAINLY NEED IT TO COME TO A HUMAN-LIKE STATE</h2>
         </div>
-        <div className="image-container_homepage">
-          <img src="#" alt="coffe mug"></img>
-        </div>
+          <img className="image-coffee-homepage" src={require('../images/coffee_nobackground.png')} alt="coffe mug"></img>
       </div>
     </div>
   );
