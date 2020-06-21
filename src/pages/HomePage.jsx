5d53cc30dbe411625a408aa09719c1da54a5b114
Lukas <lukas_weymann@hotmail.com>
2020-06-21 11:22:07 +0200

    Added and styled a persistent drawer from Material-UI in order to have menu options in a higher aesthetic design

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 import "./HomePage.css";
+import PersistenDrawerRight from "./Menu-Bar";
 
 function HomePage() {
   return (
     <div className="main-container_homepage">
-      <nav>
-        <a>HOME</a>
-        <a>ABOUT</a>
-        <a>SURPRISE ME!</a>
-        <a>PLACES</a>
-        <a>COFFEE AND SLEEP</a>
-        <a>ESSENTIALS</a>
-        <a>HOW TO BREW</a>
-      </nav>
+      <PersistenDrawerRight />
       <div className="wrapper-homepage">
         <div className="headers-homepage">
           <h1>WE LOVE CAFFEINE</h1>
           <h2>WE CERTAINLY NEED IT TO COME TO A HUMAN-LIKE STATE</h2>
         </div>
-          <img className="image-coffee-homepage" src={require('../images/coffee_nobackground.png')} alt="coffe mug"></img>
+        <img
+          className="image-coffee-homepage"
+          src={require("../images/coffee_nobackground.png")}
+          alt="coffe mug"
+        ></img>
       </div>
     </div>
   );
