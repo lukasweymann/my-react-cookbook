34ba1ebdf69a1f0587b4ba82f320f17994ff9083
Lukas <lukas_weymann@hotmail.com>
2020-06-21 10:12:38 +0200

    Added latest changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./HomePage.css";
 
+
+
+
 function HomePage() {
   return (
     <div className="main-container_homepage">
@@ -16,7 +19,7 @@ function HomePage() {
           <h2>WE CERTAINLY NEED IT TO COME TO A HUMAN-LIKE STATE</h2>
         </div>
         <div className="image-container_homepage">
-          <img src="#" alt="coffe mug"></img>
+          <img src={require('./coffee.coffeeblog.png')} alt="coffe mug"/>
         </div>
       </div>
     </div>
