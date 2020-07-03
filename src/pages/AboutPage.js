91ac56e1e9525254fc5a397c6448abb049830b54
Lukas <lukas_weymann@hotmail.com>
2020-07-03 13:48:52 +0200

    Added contactform

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from '@material-ui/core/Button';
 import "./AboutPage.css";
 import PersistenDrawerRight from "./Menu-Bar";
 
@@ -11,7 +12,7 @@ function AboutPage() {
           <h1 className="about-page__h1">ABOUT US</h1>
           <h2 className="about-page__h2">We are all about coffee. Not just the one containing caffeine, so don't be fooled by the blog's name. We love brewind, tasting and smelling coffee. We worry about quality, technique, and health issues related to coffee consumption. </h2>
           <h3 className="about-page__h3">Be sure to check the blog's links in order to learn about the aforementioned topics, find interesting products and much more. We are always looking for feedback! Be sure to email us in case you've got any suggestions to make this project better.</h3>
-          <button className="aboutpage-button">EMAIL US</button>
+          <Button variant="outlined">EMAIL US</Button>
         </div>
         <img
           className="image-coffee-aboutpage"
